a0a45edae4b6ba33b0772f68d76020549d9a4b11
aldemar <aldemarazevedo2015@hotmail.com>
2019-10-18 12:14:37 -0300

    provadoprofBruno

---
+++ public/chat.js
@@ -0,0 +1,40 @@
+
+$(function(){
+   	//conexão 
+	var socket = io.connect('http://localhost:3000')
+
+	//butões
+	var message = $("#message")
+	var username = $("#username")
+	var send_message = $("#send_message")
+	var send_username = $("#send_username")
+	var chatroom = $("#chatroom")
+	var feedback = $("#feedback")
+
+	//Emite um Mensagem
+	send_message.click(function(){
+		socket.emit('new_message', {message : message.val()})
+	})
+
+	
+	socket.on("new_message", (data) => {
+		feedback.html('');
+		message.val('');
+		chatroom.append("<p class='message'>" + data.username + ": " + data.message + "</p>")
+	})
+
+	//Emite um usuario
+	send_username.click(function(){
+		socket.emit('change_username', {username : username.val()})
+	})
+
+	
+	message.bind("keypress", () => {
+		socket.emit('typing')
+	})
+
+	
+	socket.on('typing', (data) => {
+		feedback.html("<p><i>" + data.username + " is typing a message..." + "</i></p>")
+	})
+});
